390b6e7006821adbd4d6b787195ec7ab2b93936b
outtaspacecode <cole.beebe@protonmail.com>
2026-02-15 09:53:41 -0500

    Resume data is much cleaner and more modular now

--- js/modules/templates.mjs
+++ js/modules/templates.mjs
@@ -11,7 +11,7 @@ export function resumeTemplate(data) {
                     <h2>Profile</h2>
                     <p>${data.profile}</p>
                 </section>
-                <sesction class="contact">
+                <section class="contact">
                     <h3>Phone</h3>
                     <p>${data.phone}</p>
                     <h3>Email</h3>
@@ -21,20 +21,73 @@ export function resumeTemplate(data) {
                         ${data.links.map(link => `<a href="${link.dest}" target="_blank">${link.name}</a>`).join('')}
                     </section>
                 </section>
-                <section></section>
+                <section>
+                    <h3>Additional Information</h3>
+                    <h4>Spoken Languages</h4>
+                    <ul>
+                        ${data.additionalInfo.languages.map(lang => `<li>${lang.name} (${lang.proficiency})</li>`).join('')}
+                    </ul>
+                </section>
             </div>
             <div class="divider"></div>
             <div class="main">
-                ${data.experience.map(exp => {
-                    return `
-                        <section>
-                            <h2>${exp.title}</h2>
-                            <hr />
-                            <p>${exp.content}</p>
-                        </section>
-                    `;
-                }).join('')}
+                ${educationSectionTemplate(data.experience.education)}
+                ${workSectionTemplate(data.experience.work)}
+                ${skillSectionTemplate(data.experience.skills)}
             </div>
         </div>
     `;
+}
+
+function educationSectionTemplate(data) {
+    return `
+        <section>
+            <h2>Education</h2>
+            <hr />
+            <p>${data.title}</p>
+            <p>${data.school} - ${data.location}</p>
+            <h4>Relevant Technical Projects</h4>
+            <ul>
+                ${getListItems(data.projects)}
+            </ul>
+        </section>
+    `;
+}
+
+function workSectionTemplate(data) {
+    return `
+        <section>
+            <h2>Work Experience</h2>
+            <hr />
+            ${data.map(job => `
+                <h4>${job.title}</h4>
+                <h5>${job.business} - ${job.location}</h5>
+                <p>${job.date}</p>
+                <ul>
+                    ${getListItems(job.skills)}
+                </ul>
+            `).join('')}
+        </section>
+    `;
+}
+
+function skillSectionTemplate(data) {
+    return `
+        <section>
+            <h2>Technical Skills</h2>
+            <hr />
+            <h4>Languages</h4>
+            <ul>
+                ${getListItems(data.languages)}
+            </ul>
+            <h4>Frameworks</h4>
+            <ul>
+                ${getListItems(data.frameworks)}
+            </ul>
+        </section>
+    `;
+}
+
+function getListItems(data) {
+    return data.map(item => `<li>${item}</li>`).join('');
 }
